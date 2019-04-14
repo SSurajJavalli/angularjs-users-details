8259bd25dbb5b2404eeddf28146f378c84813b3a
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-15 00:14:55 +0200

    Finished Edit Users Details

--- src/modules/user/users-detials/users-detials.js
+++ src/modules/user/users-detials/users-detials.js
@@ -9,15 +9,34 @@ function Directive() {
 }
 
 // Controller
-Controller.$inject = ["$scope", "$routeParams", "UsersData", "UsersAPI"];
-function Controller($scope, $routeParams, UsersData, UsersAPI) {
+Controller.$inject = ["$scope", "$routeParams", "$location", "UsersData", "UsersAPI"];
+function Controller($scope, $routeParams, $location, UsersData, UsersAPI) {
   $scope.user = {};
+  $scope.saveUser = saveUser;
+  $scope.checkEdits = checkEdits;
+  $scope.cancelEdits = cancelEdits;
 
   this.$onInit = function() {
     UsersAPI.getUserById($routeParams.id).then(function(response) {
       $scope.user = response.data;
-    })
+    });
   };
+
+  // Save User
+  function saveUser() {}
+
+  // Cancel Edits
+  function checkEdits() {
+    if ($scope.editUser.$dirty) {
+      jQuery("#cancelUserEdit").modal("show");
+    }
+  }
+
+  // Cancel Edits
+  function cancelEdits() {
+    jQuery("#cancelUserEdit").modal("hide");
+    $location.url('/users');
+  }
 }
 
 module.exports.Directive = Directive;
