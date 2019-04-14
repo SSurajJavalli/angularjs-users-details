175d086ca78b395002dafadbe23d76cf68eb85f5
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-15 01:15:41 +0200

    Implemented Json2Csv and download

--- src/modules/helpers/services/json2CSV.service.js
+++ src/modules/helpers/services/json2CSV.service.js
@@ -8,7 +8,30 @@
   function Json2Csv() {
     // Json2Csv JSON data as CSV
     this.download = function(data) {
-      console.log(data);
+      if (!data.length) return;
+
+      // Format Data
+      let titles = Object.keys(data[0]).join(",") + "\r\n";
+
+      let content = data.map(row => Object.values(row).join(",")).join("\r\n");
+
+      let csv = titles + content;
+
+      // Create CSV file and Download
+      let link = document.createElement("a");
+      link.id = "lnkDwnldLnk";
+      document.body.appendChild(link);
+
+      let blob = new Blob([csv], { type: "text/csv" });
+      let csvUrl = window.webkitURL.createObjectURL(blob);
+      let filename = "UserExport.csv";
+      jQuery("#lnkDwnldLnk").attr({
+        download: filename,
+        href: csvUrl
+      });
+
+      jQuery("#lnkDwnldLnk")[0].click();
+      document.body.removeChild(link);
     };
   }
 })();
